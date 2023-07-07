a9eaa8f402d78924cbe5e61997964509f46aac8f
Alexey <kalnitskiy2018@mail.ru>
2023-07-07 21:03:35 +0500

    Упражнение №3

---
+++ ex3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2 == 0){
+    Console.WriteLine("Число четное");
+}
+else if(num%2 == 1){
+    Console.WriteLine("Число нечетное");
+}
