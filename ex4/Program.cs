7055a36f632e694ab27184d3573617d02f0d51ed
Alexey <kalnitskiy2018@mail.ru>
2023-07-07 21:06:55 +0500

    Упражнение 4

---
+++ ex4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while(i <= N){
+    if(i%2 == 0){
+        Console.WriteLine(i);
+    }
+    i++;
+}
